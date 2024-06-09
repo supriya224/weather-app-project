7a343d661d548093c993f08732d54561a4490ae8
supriya224 <52038704+supriya224@users.noreply.github.com>
2024-06-09 16:45:04 +0530

    commit changes

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -10,6 +10,7 @@ function Header() {
           width={100}
           src="https://www.kraftshala.com/static/91fae91e429948d7db90a40999c96093/83b9e/new_nav_logo.webp"
           alt=""
+          loading="lazy"
         />
 
         <ul className="flex gap-12">
